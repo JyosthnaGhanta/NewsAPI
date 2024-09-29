31e7f8eb99e4168519b878d53c685064c7538366
JyosthnaGhanta <153896263+JyosthnaGhanta@users.noreply.github.com>
2024-09-29 23:32:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const searchButton = document.getElementById('search-button');
 
 async function fetchRandomNews() {
     try {
-        const apiUrl = https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=10&apiKey=${apiKey};
+        const apiUrl = `https://newsapi.org/v2/top-headlines?sources=techcrunch&pageSize=10&apiKey=${apiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
@@ -17,7 +17,7 @@ async function fetchRandomNews() {
 
 async function fetchNewsQuery(query) {
     try {
-        const apiUrl = https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey};
+        const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey}`;
         const response = await fetch(apiUrl);
         const data = await response.json();
         return data.articles;
