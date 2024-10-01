a5dd2669dea3d04d518f3f45e4cc125d44d5ea5b
JyosthnaGhanta <153896263+JyosthnaGhanta@users.noreply.github.com>
2024-10-01 12:37:53 +0530

    index.js

--- index.js
+++ index.js
@@ -72,6 +72,136 @@ const apiData = {
             "publishedAt": "2024-09-30T01:35:00Z",
             "content": "Chinese stocks extended one of their most remarkable turnarounds in history."
         }
+    {
+        "source": {
+            "id": null,
+            "name": "Parade"
+        },
+        "author": "Emily Laurence",
+        "title": "‘I’m an Oncologist—This Is the Afternoon Snack I Eat Almost Every Day for Cancer Prevention’",
+        "description": "Here's how you can lower your risk of cancer through what you eat.",
+        "url": "https://parade.com/health/best-snack-for-cancer-prevention-according-to-an-oncologist",
+        "urlToImage": "https://media.zenfs.com/en/parade_250/efa359da870577b320877bcae8fe7582",
+        "publishedAt": "2024-09-30T19:25:00Z",
+        "content": "While certainly no one can control the fate of their health completely, there is a lot you can do to make the odds of experiencing a serious health condition a lot less likely—including cancer. The Wo… [+5192 chars]"
+    },
+    {
+        "source": {
+            "id": "espn",
+            "name": "ESPN"
+        },
+        "author": "ESPN",
+        "title": "MLB playoffs preview: World Series odds, keys to success and predicted date of doom for all 12 teams",
+        "description": "Have World Series or bust hopes for the 2024 postseason? Here's why your squad could be playing into November -- or make an early exit.",
+        "url": "https://www.espn.com/mlb/story/_/id/41398705/2024-mlb-playoffs-preview-world-series-odds-matchup-keys-tips",
+        "urlToImage": "https://a2.espncdn.com/combiner/i?img=%2Fphoto%2F2024%2F0929%2Fmlb_mega_playoff_cr_16x9.jpg",
+        "publishedAt": "2024-09-30T23:21:22Z",
+        "content": "American League\r\nNo. 1 seed | 94-66 | AL East champs\r\nALDS opponent: Royals/Orioles (62% chance of advancing)\r\nWorld Series odds: 19.4% | ESPN BET odds: +425\r\nPredicted date of their last game: Nov. … [+38349 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Javier Pastor",
+        "title": "Apple no participará en la nueva ronda de inversión de OpenAI, según el WSJ. Tiene buenos motivos",
+        "description": "La ronda de inversión de OpenAI podría cerrarse estos días. Se espera que la firma levante 6.500 millones de dólares y que su valoración estimada alcance unos espectaculares 150.000 millones de dólares.",
+        "url": "https://www.xataka.com/empresas-y-economia/apple-no-participara-ronda-inversion-openai-wsj-raro-hubiera-sido-contrario",
+        "urlToImage": "https://i.blogs.es/4db5c1/tim-sam/840_560.jpeg",
+        "publishedAt": "2024-09-30T11:42:14Z",
+        "content": "La ronda de inversión de OpenAI podría cerrarse estos días. Se espera que la firma levante 6.500 millones de dólares y que su valoración estimada alcance unos espectaculares 150.000 millones de dólar… [+2339 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Ricardo Aguilar",
+        "title": "Lanzar el iPhone más delgado de la historia antes que el primer iPhone plegable: un movimiento con sentido",
+        "description": "2025 apunta a ser un gran año para Apple. Este ha sido un 2024 de continuismo, uno en el que los iPhone 16 y iPhone 16 Pro han llegado al mercado como evoluciones discretas respecto a los modelos anteriores.",
+        "url": "https://www.xataka.com/empresas-y-economia/lanzar-iphone-delgado-historia-primer-iphone-plegable-movimiento-sentido",
+        "urlToImage": "https://i.blogs.es/4e5be3/iphone/840_560.jpeg",
+        "publishedAt": "2024-09-30T07:30:36Z",
+        "content": "2025 apunta a ser un gran año para Apple. Este ha sido un 2024 de continuismo, uno en el que los iPhone 16 y iPhone 16 Pro han llegado al mercado como evoluciones discretas respecto a los modelos ant… [+2350 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Enrique Pérez",
+        "title": "Epic Games vuelve a denunciar por bloquear a terceros. Tras Apple y Google, ahora le toca a Samsung",
+        "description": "Epic Games vuelve a la carga. Tras su larga batalla legal contra Apple ahora ha decidido presentar una nueva demanda.",
+        "url": "https://www.xataka.com/legislacion-y-derechos/epic-games-vuelve-a-denunciar-bloquear-a-terceros-apple-google-ahora-le-toca-a-samsung",
+        "urlToImage": "https://i.blogs.es/c1ddf9/epic-games/840_560.jpeg",
+        "publishedAt": "2024-09-30T13:26:58Z",
+        "content": "Epic Games vuelve a la carga. Tras su larga batalla legal contra Apple ahora ha decidido presentar una nueva demanda. La empresa detrás de juegos como 'Fortnite' y desarrollador del motor Unreal Engi… [+2085 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Ricardo Aguilar",
+        "title": "He probado los iPhone 16 y 16 Pro. Tengo clarísimo cuál me compraría",
+        "description": "Un año más, la pregunta a la hora de comprar un iPhone está en si apostar o no por el modelo Pro. Este año hay algunas diferencias que no encontrábamos en generaciones anteriores.",
+        "url": "https://www.xataka.com/analisis/iphone-16-vs-iphone-16-plus-vs-iphone-16-pro-vs-iphone-16-pro-max-comparativa",
+        "urlToImage": "https://i.blogs.es/564efa/iphone-16/840_560.jpeg",
+        "publishedAt": "2024-09-30T14:00:52Z",
+        "content": "Un año más, la pregunta a la hora de comprar un iPhone está en si apostar o no por el modelo Pro. Este año hay algunas diferencias que no encontrábamos en generaciones anteriores… [+11350 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Javier Pastor",
+        "title": "Mark Zuckerberg apuesta por EssilorLuxottica. Según él, puede llegar a ser la Samsung europea",
+        "description": "El éxito de las Ray-Ban Meta ha tenido un efecto sorpresa para muchos: EssilorLuxottica, el mayor fabricante de gafas del mundo, captó el interés de Meta y Google.",
+        "url": "https://www.xataka.com/wearables/mark-zuckerberg-apuesta-essilorluxottica-puede-llegar-a-ser-samsung-europea",
+        "urlToImage": "https://i.blogs.es/dbd284/essilor/840_560.jpeg",
+        "publishedAt": "2024-09-30T09:01:39Z",
+        "content": "El éxito de las Ray-Ban Meta ha tenido un efecto sorpresa para muchos: EssilorLuxottica, el mayor fabricante de gafas del mundo, acabó captando el interés de la empresa de Mark Zuckerberg —y el de Goo… [+2563 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Rubén Andrés",
+        "title": "Steve Jobs afrontaba los problemas más complicados con un paseo de 10 minutos. La ciencia está demostrando que tenía razón",
+        "description": "Steve Jobs took 10-minute walks to solve complex problems. Science is now proving this method right.",
+        "url": "https://www.xataka.com/medicina-y-salud/steve-jobs-afrontaba-problemas-complicados-paseo-10-minutos-ciencia-esta-demostrando-que-tenia-razon",
+        "urlToImage": "https://i.blogs.es/ec42ec/caminar-steve-jobs/840_560.jpeg",
+        "publishedAt": "2024-09-30T16:30:47Z",
+        "content": "Una cita atribuida a Picasso decía: \"La inspiración existe, pero te tiene que encontrar trabajando\". Steve Jobs took walks to unblock his mind and solve challenges, and science is showing how small w… [+4022 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Xataka.com"
+        },
+        "author": "Enrique Pérez",
+        "title": "Ya puedes ver 'La caída de la casa Usher' en Netflix, aunque tienes otras tres series muy recomendables antes del estreno",
+        "description": "Análisis de la serie 'La caída de la casa Usher', ya disponible en Netflix. Además, recomendaciones de otras series para ver antes de su estreno.",
+        "url": "https://www.xataka.com/cine-y-tv/ya-puedes-ver-caida-casa-usher-netflix-otras-tres-series-muy-recomendables-antes-estreno",
+        "urlToImage": "https://i.blogs.es/f8e07b/la-caida/840_560.jpeg",
+        "publishedAt": "2024-09-30T09:26:38Z",
+        "content": "Ha llegado el otoño y con él la nueva serie de Netflix basada en 'La caída de la casa Usher', el icónico relato de Edgar Allan Poe. Mientras tanto, aquí tienes tres series de terror recomendadas para … [+2568 chars]"
+    },
+    {
+        "source": {
+            "id": null,
+            "name": "Forbes"
+        },
+        "author": "David Phelan",
+        "title": "Apple Leaks Detail Stunning New iPhone Feature",
+        "description": "Apple's new iPhone feature is set to change the game for users worldwide, bringing innovations that push the limits of current technology.",
+        "url": "https://www.forbes.com/sites/davidphelan/2024/09/30/apple-leaks-detail-stunning-new-iphone-feature/",
+        "urlToImage": "https://imageio.forbes.com/specials-images/imageserve/5f5f3e8df28745be6a515c7b/960x0.jpg",
+        "publishedAt": "2024-09-30T10:00:00Z",
+        "content": "This new iPhone feature will allow users to experience enhanced AI-driven functionalities, pushing smartphone capabilities to new limits. This is a part of Apple's broader push towards integrating AI… [+2540 chars]"
+    }
     ]
 };
 
